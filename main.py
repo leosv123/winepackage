395ba0ea15b1071a4bcf0af87090f3535c0077dc
Lingraj S Vannur <lingrajsvannur@lingrajs-mbp.ds.usfca.edu>
2023-08-18 16:49:48 -0700

    model trainer  added

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from wineproj import logger
 from wineproj.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from wineproj.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from wineproj.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+from wineproj.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
 
 STAGE_NAME = "Data Ingestion Stage"
 try:
@@ -22,3 +24,23 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+STAGE_NAME = "Data Transformation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataTransformationTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME = "Model Training stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = ModelTrainerTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
