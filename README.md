e88e42952896c230bd202d03719a45f3ccd1b288
Lingraj S Vannur <lingrajsvannur@lingrajs-mbp.ds.usfca.edu>
2023-08-18 18:07:01 -0700

    model evaluation added

--- README.md
+++ README.md
@@ -11,3 +11,21 @@
 7. Update the pipeline
 8. Update the main.py
 9. Update the components
+
+
+## cmd
+- mlflow ui
+
+## DagsFlow Tracking UI
+
+MLFLOW_TRACKING_URI=https://dagshub.com/leosv123/winepackage.mlflow \
+MLFLOW_TRACKING_USERNAME=leosv123 \
+MLFLOW_TRACKING_PASSWORD=4d2cdefe97fd614c06a2f1e964167661d3329e05 \
+python script.py
+
+
+## bash
+
+export MLFLOW_TRACKING_URI=https://dagshub.com/leosv123/winepackage.mlflow
+export MLFLOW_TRACKING_USERNAME=leosv123
+MLFLOW_TRACKING_PASSWORD=4d2cdefe97fd614c06a2f1e964167661d3329e05
--- main.py
+++ main.py
@@ -3,27 +3,33 @@ from wineproj.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipel
 from wineproj.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from wineproj.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 from wineproj.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
+from wineproj.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
 
-STAGE_NAME = "Data Ingestion Stage"
+
+STAGE_NAME = "Data Ingestion stage"
 try:
-    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-    obj = DataIngestionTrainingPipeline()
-    obj.main()
-    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
-    logger.exception(e)
-    raise e
+        logger.exception(e)
+        raise e
 
 
-STAGE_NAME = "Data Validation Stage"
+
+
+STAGE_NAME = "Data Validation stage"
 try:
-    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-    obj = DataValidationTrainingPipeline()
-    obj.main()
-    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataValidationTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
-    logger.exception(e)
-    raise e
+        logger.exception(e)
+        raise e
+
+
 
 STAGE_NAME = "Data Transformation stage"
 try:
@@ -35,10 +41,25 @@ except Exception as e:
         logger.exception(e)
         raise e
 
-STAGE_NAME = "Model Training stage"
+
+
+
+STAGE_NAME = "Model Trainer stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = ModelTrainerTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+
+STAGE_NAME = "Model evaluation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = ModelEvaluationTrainingPipeline()
    data_ingestion.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
